46d5009ebb7af44498c5face0b6ea5871c9cff2b
skye1993 <amy_macpherson14@hotmail.co.uk>
2025-11-10 14:22:19 +0000

    Added  Data Grid file

---
+++ nc-news/src/Components/Data Grid.jsx
@@ -0,0 +1,11 @@
+function DataGrid() {
+  return (
+    <>
+      <div className="rectangle" />
+      <div className="rectangle" />
+      <div className="rectangle" />
+      <div className="rectangle" />
+    </>
+  );
+}
+export default DataGrid;
