f72d27c3a36a871c7916fe67704023e01df2c9d0
skye1993 <amy_macpherson14@hotmail.co.uk>
2025-11-10 14:20:56 +0000

    Added Title pag

---
+++ nc-news/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header(){
+
+    const title = 'Northcoders News'
+
+
+
+return (
+    <header>
+        <h1>{title}</h1>
+        <img src='./src/nc-news.jpeg'/>
+    </header>
+)
+}
+export default Header
