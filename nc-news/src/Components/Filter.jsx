50f9367c002d9bacd320fbcad9825f7c7b22ac3e
skye1993 <amy_macpherson14@hotmail.co.uk>
2025-11-10 14:21:40 +0000

    added filter file

---
+++ nc-news/src/Components/Filter.jsx
@@ -0,0 +1,15 @@
+
+function Filter(){
+
+
+    return (
+        <>
+        <section>
+       <tag>Filter/Sort by:</tag>
+       <input type="text"></input>
+        </section>
+        </>
+    )
+}
+
+export default Filter
