c749d858cdddfd88f887c0aec04c0cb9c0579aa8
skye1993 <amy_macpherson14@hotmail.co.uk>
2025-11-11 09:04:52 +0000

    added search button

--- nc-news/src/Components/Filter.jsx
+++ nc-news/src/Components/Filter.jsx
@@ -1,15 +1,16 @@
-
-function Filter(){
-
-
-    return (
-        <>
-        <section>
-       <tag>Filter/Sort by:</tag>
-       <input type="text"></input>
-        </section>
-        </>
-    )
+function Filter() {
+  return (
+    //26 to make more controlled
+    <>
+      <form>
+       <label>
+        Filter/Sort by:
+        <input type="text"/>
+      </label>
+      <button type="submit">search</button>
+      </form>
+    </>
+  );
 }
 
-export default Filter
+export default Filter;
